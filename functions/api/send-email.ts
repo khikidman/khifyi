5fa2f6564f42cd75acbfd494bb91a5cd43ffc973
Khi Kidman <mkidma2@uic.edu>
2026-03-05 03:12:05 -0600

    Testing resend

--- functions/api/send-email.ts
+++ functions/api/send-email.ts
@@ -1,5 +1,16 @@
 import { Resend } from "resend";
 
-export async function onRequestPost(context: any) {
-  return new Response("Test working");
-}
+export default {
+  async fetch() {
+    const resend = new Resend("re_euHvFnWB_E7LztgZL8vJoExLKdQKfZuas");
+
+    const { data, error } = await resend.emails.send({
+      from: "hello@example.com",
+      to: "khi.kidman@gmail.com",
+      subject: "Hello World",
+      html: "<p>Hello from Workers</p>",
+    });
+
+    return Response.json({ data, error });
+  },
+};
